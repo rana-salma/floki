11c9b1f5a01c95289a5c461b5c4bf40792a68273
Philip Sampaio <philip.sampaio@gmail.com>
2021-02-03 02:07:26 -0300

    Remove dependency of JSON decoder and fix iodata
    This is a big change that enable us to replace the fetch of HTML
    entities from a JSON file parsed in memory to a lookup in a module.
    This is similar to what we have in HTML Entities package.
    
    Also this fixes a lot of small IO data building errors.

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Floki.Mixfile do
 
     [
       {:html_entities, "~> 0.5.0"},
-      {:jason, "~> 1.1"},
+      {:jason, "~> 1.1", only: :dev},
       {:earmark, "~> 1.2", only: :dev},
       {:ex_doc, "~> 0.23.0", only: :dev},
       {:benchee, "~> 1.0.1", only: :dev},
